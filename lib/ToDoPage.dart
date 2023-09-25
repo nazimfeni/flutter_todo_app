6b38be6eed654145449f9c8f185840c326e4e4d3
nazimfeni <mdnazimuddin77@gmail.com>
2023-09-25 21:39:13 +0600

    updateed

--- lib/ToDoPage.dart
+++ lib/ToDoPage.dart
@@ -8,7 +8,29 @@ class ToDoPage extends StatefulWidget {
 }
 
 class _ToDoPageState extends State<ToDoPage> {
-  List ToDoList =[{"1":"1"},{"2":"2"}];
+  List ToDoList =[];
+  String item ="";
+
+  MyInputOnChange(content){
+    setState(() {
+      item = content;
+    });
+  }
+AddItem(){
+   setState(() {
+     ToDoList.add({'item':item});
+   });
+
+
+}
+
+RemoveItem(index){
+
+   setState(() {
+     ToDoList.removeAt(index);
+   });
+
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +43,10 @@ class _ToDoPageState extends State<ToDoPage> {
                 flex: 10,
                 child: Row(
                   children: [
-                    Expanded(flex: 70, child: TextFormField(decoration: AppInputDecoration("List Item"),)),
-                    Expanded(flex:20, child:Padding(padding: EdgeInsets.only(left: 5),child: ElevatedButton(onPressed: (){}, child: Text("Add"),style: AppButtonStyle(),),)),
+                    Expanded(flex: 70, child: TextFormField( onChanged:(content){
+                    MyInputOnChange(content);
+                    },decoration: AppInputDecoration("List Item"),)),
+                    Expanded(flex:20, child:Padding(padding: EdgeInsets.only(left: 5),child: ElevatedButton(onPressed: (){AddItem();}, child: Text("Add"),style: AppButtonStyle(),),)),
                   ],
                 )
             ),
@@ -35,8 +59,8 @@ class _ToDoPageState extends State<ToDoPage> {
                         child: sizeBox50(
                             Row(
                                 children: [
-                                  Expanded(flex: 80, child: Text("Item")),
-                                  Expanded(flex:20,child: TextButton(onPressed: (){}, child: Icon(Icons.delete))),
+                                  Expanded(flex: 80, child: Text(ToDoList[index]['item'].toString())),
+                                  Expanded(flex:20,child: TextButton(onPressed: (){RemoveItem(index);}, child: Icon(Icons.delete))),
                                 ]
 
                             )
